c3a9c95826933488b182b0618921542c45a5a7ff
Meday98 <mohamedayad.work@gmail.com>
2025-08-09 19:12:12 +0200

    Complete Sign Up Page Functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,33 +1,75 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Signup() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/sign-in");
+    } catch (error) {
+      setLoading(false);
+      setError("An error occurred while signing up. Please try again.");
+      console.error("Error during signup:", error);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="flex flex-col items-center">
           <input
             type="text"
             placeholder="Username"
             className="border border-gray-300 rounded-lg p-3 mb-4 w-80"
             id="username"
+            onChange={handleChange}
           />
           <input
             type="email"
             placeholder="Email"
             className="border border-gray-300 rounded-lg p-3 mb-4 w-80"
             id="email"
+            onChange={handleChange}
           />
           <input
             type="password"
             placeholder="Password"
             className="border border-gray-300  rounded-lg p-3 mb-4 w-80"
             id="password"
+            onChange={handleChange}
           />
           <button
+            disabled={loading}
             type="submit"
             className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-200">
-            Sign Up
+            {loading ? "Loading..." : "Sign Up"}
           </button>
         </div>
       </form>
@@ -37,6 +79,11 @@ export default function Signup() {
           <span>Sign in</span>
         </Link>
       </div>
+      {error && (
+        <div className="text-red-500 text-center mt-4">
+          <p>{error}</p>
+        </div>
+      )}
     </div>
   );
 }
